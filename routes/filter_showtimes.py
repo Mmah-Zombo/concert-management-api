32dd15fe545f285e9bf507b3086ef788e693b30a
josephine-annmarie-magona <josephinemagona62@gmail.com>
2025-06-24 04:54:39 +0000

    Added All The Filter Files In The route Directory

---
+++ routes/filter_showtimes.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from database import get_db
+from crud.filter import filter_showtime_by_date
+
+router = APIRouter(prefix="/showtimes", tags=["Showtimes"])
+
+
+
+@router.get("/filter/date")
+def filter_by_date(start: datetime, end: datetime, db: Session = Depends(get_db)):
+    return filter_showtime_by_date(db, start, end)
